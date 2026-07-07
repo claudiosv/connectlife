7e937a0aecbb9b2b261dac17afe953a64fec5723
Claudio Spiess <claudiosv@users.noreply.github.com>
2026-07-06 20:42:58 -0700

    fix handler
    Signed-off-by: Claudio Spiess <claudiosv@users.noreply.github.com>

--- const.py
+++ const.py
@@ -1,6 +1,6 @@
 """Constants for the ConnectLife integration."""
 
-DOMAIN = "connectlife_claudio"
+DOMAIN = "connectlife_claudio_wrapper"
 
 CONF_BEEPING = "beeping"
 CONF_DEVICES_CONFIG = "devices_config"
